b33ab1ab7e99204f0886dd634a053b5cbca79455
Scott Bren <smbren@ncsu.edu>
2019-02-06 17:50:27 -0500

    fixed a bug in movement code

--- lambdatreasure/src/App.js
+++ lambdatreasure/src/App.js
@@ -12,21 +12,25 @@ class App extends Component {
     this.state = {
 
       roomInfo: {
+
         room_id: '',
+        title: '',
+        description: '',
         coordinates: '',
+        players: [],
         items: [],
         exits: [],
         cooldown: undefined,
+        errors: [],
+        messages: []
 
       },
   
     };
   }
 
-
   componentDidMount() {
     axios
-
       .get("https://lambda-treasure-hunt.herokuapp.com/api/adv/init/", 
 
       { headers: 
@@ -43,17 +47,52 @@ class App extends Component {
 
   }
 
+  move = (e, direction) => {
+    e.preventDefault();
+  
+    axios
+      .post(`https://lambda-treasure-hunt.herokuapp.com/api/adv/move/`, 
+
+      { "direction": direction },
+      
+      { headers: 
+        { Authorization: "Token 421139965c881b1e9ffe024b6233b338a12760f4" }, 
+      },
+
+      )
+
+      .then(response => {
+        this.setState({roomInfo: response.data})
+      })
+
+      .catch(error => {
+        console.error("Invalid move.", error);
+      })
+  }
+
+
+
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+
           <p>
             You are standing in room {this.state.roomInfo.room_id}.<br />
             Choose a direction to explore the next room.<br />
-            Possible exits include: {this.state.roomInfo.exits}
+            Possible exits include: {this.state.roomInfo.exits}<br />
+            {this.state.roomInfo.cooldown}
           </p>
+
+
+          <button onClick={e => {this.move(e, "n");}}>Move North</button>
+          <button onClick={e => {this.move(e, "e");}}>Move East</button>
+          <button onClick={e => {this.move(e, "s");}}>Move South</button>
+          <button onClick={e => {this.move(e, "w");}}>Move West</button>
+
+
         
         </header>
       </div>
