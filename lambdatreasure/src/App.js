55695c6cb89b101516f2819c03d336e099eb98d4
Scott Bren <smbren@ncsu.edu>
2019-02-06 18:46:00 -0500

    manual movement is completed, added partial support for additional commands

--- lambdatreasure/src/App.js
+++ lambdatreasure/src/App.js
@@ -25,6 +25,21 @@ class App extends Component {
         messages: []
 
       },
+
+      playerInfo: {
+
+        name: '',
+        cooldown: undefined,
+        encumbrance: undefined,  // How much are you carrying?
+        strength: undefined,  // How much can you carry?
+        speed: undefined,  // How fast do you travel?
+        gold: undefined,
+        inventory: [],
+        status: [],
+        errors: [],
+        messages: []
+
+      }
   
     };
   }
@@ -70,6 +85,73 @@ class App extends Component {
       })
   }
 
+  status = (e) => {
+    e.preventDefault();
+  
+    axios
+      .post(`https://lambda-treasure-hunt.herokuapp.com/api/adv/status/`, 
+      
+      { headers: 
+        { Authorization: "Token 421139965c881b1e9ffe024b6233b338a12760f4" }, 
+      },
+
+      )
+
+      .then(response => {
+        this.setState({playerInfo: response.data})
+      })
+
+      .catch(error => {
+        console.error("Error getting player data.", error);
+      })
+  }
+
+  take = (e) => {
+    e.preventDefault();
+  
+    axios
+      .post(`https://lambda-treasure-hunt.herokuapp.com/api/adv/take/`, 
+
+      { name: "Small Treasure" },
+      
+      { headers: 
+        { Authorization: "Token 421139965c881b1e9ffe024b6233b338a12760f4" }, 
+      },
+
+      )
+
+      .then(response => {
+        this.setState({playerInfo: response.data})
+      })
+
+      .catch(error => {
+        console.error("Couldn't take item", error);
+      })
+  }
+
+  drop = (e) => {
+    e.preventDefault();
+  
+    axios
+      .post(`https://lambda-treasure-hunt.herokuapp.com/api/adv/drop/`, 
+
+      { name: "Small Treasure" },
+      
+      { headers: 
+        { Authorization: "Token 421139965c881b1e9ffe024b6233b338a12760f4" }, 
+      },
+
+      )
+
+      .then(response => {
+        this.setState({playerInfo: response.data})
+      })
+
+      .catch(error => {
+        console.error("Couldn't drop item.", error);
+      })
+  }
+
 
 
   render() {
@@ -82,15 +164,25 @@ class App extends Component {
           <p>
             You are standing in room {this.state.roomInfo.room_id}.<br />
             Choose a direction to explore the next room.<br />
-            Possible exits include: {this.state.roomInfo.exits}<br />
-            {this.state.roomInfo.cooldown}
+            {this.state.roomInfo.messages}<br />
+            {this.state.roomInfo.title}<br />
+            {this.state.roomInfo.items}<br />
+            {this.state.playerInfo.inventory}<br />
+            {this.state.roomInfo.description}<br />
+            Possible exits: {this.state.roomInfo.exits}<br />
+            You'll need to rest for {this.state.roomInfo.cooldown} second(s) before you have the strength to move again.<br />
           </p>
 
 
           <button onClick={e => {this.move(e, "n");}}>Move North</button>
           <button onClick={e => {this.move(e, "e");}}>Move East</button>
           <button onClick={e => {this.move(e, "s");}}>Move South</button>
-          <button onClick={e => {this.move(e, "w");}}>Move West</button>
+          <button onClick={e => {this.move(e, "w");}}>Move West</button><br />
+
+          <button onClick={e => {this.status(e);}}> Check status </button><br />
+
+          <button onClick={e => {this.take(e);}}> Take Item </button><br />
+          <button onClick={e => {this.drop(e);}}> Drop Item </button><br />
 
 
         
